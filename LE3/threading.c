72f88330b7860034f9715a72a749c39481d4ca03
Cody Wu <codywu@Codys-MacBook-Air-7.local>
2024-10-04 19:26:26 -0500

    first

---
+++ LE3/threading.c
@@ -0,0 +1,21 @@
+#include <threading.h>
+
+void t_init()
+{
+        // TODO
+}
+
+int32_t t_create(fptr foo, int32_t arg1, int32_t arg2)
+{
+        // TODO
+}
+
+int32_t t_yield()
+{
+        // TODO
+}
+
+void t_finish()
+{
+        // TODO
+}
